99da8b3aac89fd398a556d274fba3e52fd605307
Kenan <mckenzie.kenan@yahoo.com>
2022-12-07 16:05:39 -0500

    added Role constructor in lib/ and addRole function in index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const inquirer = require('inquirer');
 const db = require('./db/connection');
 const cTable = require('console.table');
 const Department = require('./lib/Department');
+const Role = require('./lib/Role')
+const Rx = require('rx')
 
+function init() {
+let departmentArray = ['Sales', 'Engineering', 'Finance', 'Legal']
 function mainMenu(){
     inquirer.prompt([
         {
@@ -61,14 +65,53 @@ function addDepartment() {
             type: 'input',
             name: 'departmentName',
             message: 'What is the name of the department you would like to add?'
-        }.then(response) => {
-            let department = new Department(response.name, )
         }
-    ])
+    ]).then((response) => {
+        let department = new Department(response.name)
+        departmentArray.push(department)
+    })    
 }
 
 function addRole() {
 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the title of the role you would like to add?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'Please enter a salary for this role:',
+            validate(value) {
+                const valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+              },
+              filter: Number,
+        },
+        {
+            type: 'input',
+            name: 'roleDept',
+            message: 'What department will this role be in?',
+            validate: roleDept => {
+                if (departmentArray.indexOf(roleDept) > -1) {
+                    return true
+                } else {
+                    console.log('Please enter a valid department.')
+                    return false;
+                }
+            }
+        }
+    ]).then((response) => {
+        const sql = `INSERT INTO roles(title, salary, departmet_id) VALUES (?,?,?)`;
+        const role = new Role(response.title, response.salary, response.department_id)
+        db.query(sql, role, (err, res) => {
+            if (err) throw err;
+            console.log('Role added')
+        })
+        mainMenu()
+    })
 } 
 
 function addEmployee() {
@@ -78,3 +121,6 @@ function addEmployee() {
 function updateRole() {
 
 }
+}
+
+l
